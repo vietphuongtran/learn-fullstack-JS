e3c416d13220bd09b7979d87a5981b8150eacbb2
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-24 17:02:39 -0400

    2.3 complete

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-//import config, { nodeEnv, logStars } from './config';
-//
-//console.log(config, nodeEnv);
-//
-//logStars('Function');
-//import https from 'https';
-//get the status code of lynda.com and its data
-//https.get('https://www.lynda.com', res => {
-//    console.log('Status code ', res.statusCode);
-//    res.on('data', chunk => {
-//        console.log(chunk.toString());
-//    });
-//});
-//import http from 'http';
-//const server = http.createServer();
-//server.on('request', (req, res) => {
-//    res.write('Hello HTTP!\n');
-//    setTimeout(() => {
-//    res.write('I can stream!\n');
-//    res.end();
-//  }, 3000);
-//});
-//server.listen(8080);
 import express from 'express';
+//mean using filesystem
+//import fs from 'fs';
 import config from './config';
+import apiRouter from './api';
 
 const server = express();
+server.get("/", (req, res) =>  {
+    //send the hello express to the main page
+    res.send("Hello world from Express");
+});
+
+//built in express method to display the about.html
+//meaning: use this file in the public folder
+server.use(express.static('public'));
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+//server.get("/about.html", (req, res) =>  {
+//    //send the hello express to the about.html
+//    //res.send("Hello world from Express in about page");
+//    //send the file using filesystem
+//    fs.readFile('./about.html', (err, data) => {
+//        res.send(data.toString());
+//    });
+//}); 
+
 server.listen(config.port, () => {
     console.info('Express is now on port '+ config.port);
 });

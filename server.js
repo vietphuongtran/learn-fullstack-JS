149eea708b50230c78d90f1978c56fc82810a224
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-24 17:37:30 -0400

    chapter 2 complete

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ import config from './config';
 import apiRouter from './api';
 
 const server = express();
+//this is the line to set up EJS
+//EJS: Embeded JavaScript, a technique for writing HTMl with JS https://ejs.co/
+server.set('view engine', 'ejs');
+//server.get("/", (req, res) =>  {
+//    //send the hello express to the main page
+//    res.send("Hello world from Express");
+//});
 server.get("/", (req, res) =>  {
-    //send the hello express to the main page
-    res.send("Hello world from Express");
+    //res.render send the ejs to the index page
+    res.render("index", {
+        //to export it as a string use <%= %>
+        content: "Hello, this is Ejs file written in the server",
+        //to export it as Html tag <%- %>
+        anotherContent: "This content includes <div>div tag</div>"
+    });
 });
-
-//built in express method to display the about.html
-//meaning: use this file in the public folder
 server.use(express.static('public'));
 server.use('/api', apiRouter);
-server.use(express.static('public'));
-
-//server.get("/about.html", (req, res) =>  {
-//    //send the hello express to the about.html
-//    //res.send("Hello world from Express in about page");
-//    //send the file using filesystem
-//    fs.readFile('./about.html', (err, data) => {
-//        res.send(data.toString());
-//    });
-//}); 
+server.use(express.static('public')); 
 
 server.listen(config.port, () => {
     console.info('Express is now on port '+ config.port);
